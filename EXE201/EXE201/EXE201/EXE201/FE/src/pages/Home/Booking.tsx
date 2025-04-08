e375f216359e7d09c234fa9ef80c7c66a5a063cc
EmBerrrrrr <lan18051973@gmail.com>
2025-04-08 14:48:49 +0700

    chill guy

--- EXE201/EXE201/EXE201/EXE201/FE/src/pages/Home/Booking.tsx
+++ EXE201/EXE201/EXE201/EXE201/FE/src/pages/Home/Booking.tsx
@@ -4,6 +4,7 @@ import axios from "axios";
 import { motion } from "framer-motion";
 import Layout from "../../layout/Layout";
 import { Divider } from "antd";
+import { Link } from "react-router-dom";
 
 interface Service {
   _id: string;
@@ -52,7 +53,9 @@ const Booking: React.FC = () => {
   const fetchServices = async () => {
     setLoading(true);
     try {
-      const response = await axios.get("https://exe201-production.up.railway.app/api/products");
+      const response = await axios.get(
+        "https://exe201-production.up.railway.app/api/products"
+      );
       setServices(response.data);
     } catch (error) {
       console.error("Error fetching services:", error);
@@ -63,7 +66,9 @@ const Booking: React.FC = () => {
 
   const fetchCategories = async () => {
     try {
-      const response = await axios.get("https://exe201-production.up.railway.app/api/categories");
+      const response = await axios.get(
+        "https://exe201-production.up.railway.app/api/categories"
+      );
       setCategories(response.data);
     } catch (error) {
       console.error("Error fetching categories:", error);
@@ -94,20 +99,27 @@ const Booking: React.FC = () => {
                   key={service._id}
                   className="bg-white p-6 rounded-lg shadow-lg flex flex-col items-center text-center"
                 >
-                  <img
-                    className="w-full h-48 object-cover rounded-lg mb-4"
-                    src={service.image || "/default-image.jpg"}
-                    alt={service.name}
-                  />
-                  <h3 className="text-2xl font-semibold text-gray-800 mb-4">
-                    {service.name}
-                  </h3>
-                  <p className="font-semibold text-gray-800 mb-4">
-                    {service.description}
-                  </p>
-                  <p className="text-lg font-bold text-gray-900 mb-2">
-                    {service.price ? `${(service.price).toLocaleString("vi-VN")} VNĐ` : "Liên hệ"}
-                  </p>
+                  <Link
+                    to={`/booking_services/${service._id}`}
+                    className="w-full"
+                  >
+                    <img
+                      className="w-full h-48 object-cover rounded-lg mb-4"
+                      src={service.image || "/default-image.jpg"}
+                      alt={service.name}
+                    />
+                    <h3 className="text-2xl font-semibold text-gray-800 mb-4">
+                      {service.name}
+                    </h3>
+                    <p className="font-semibold text-gray-800 mb-4">
+                      {service.description}
+                    </p>
+                    <p className="text-lg font-bold text-gray-900 mb-2">
+                      {service.price
+                        ? `${service.price.toLocaleString("vi-VN")} VNĐ`
+                        : "Liên hệ"}
+                    </p>
+                  </Link>
                 </motion.div>
               ))}
             </div>
@@ -133,7 +145,9 @@ const Booking: React.FC = () => {
               key={category._id}
               onClick={() => setSelectedCategory(category._id)}
               className={`w-full px-6 py-2 text-lg font-semibold border-b ${
-                selectedCategory === category._id ? "bg-[#00B389] text-white" : ""
+                selectedCategory === category._id
+                  ? "bg-[#00B389] text-white"
+                  : ""
               } rounded mb-4`}
             >
               {category.name}
